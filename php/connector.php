1be177f84d5a799594db3108502ba258239fb82e
Diego Armando Lira <darmando.lira@hotmail.com>
2016-02-12 23:49:25 -0600

    se quitaron comentario
    se eliminaron unos comentarios que no valia la pena mostrar. (:

--- php/connector.php
+++ php/connector.php
@@ -1,9 +1,9 @@
 <?php
 function getConnection() {
 	$dbhost="localhost";
-	$dbuser="desarr13_ejemplo";
-	$dbpass="huevos1";
-	$dbname="desarr13_bdSelectsAnidados";
+	$dbuser="";
+	$dbpass="";
+	$dbname="";
 	$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);	
 	$dbh -> exec("set names utf8");
 	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
